37e92c1d23055b4704d7f275a3ac1d0bb9bb0ed6
Aingaran Thirunadarajah <email@aingaran.com>
2023-07-21 16:18:16 -0400

    updated constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
-// export const HOST = 'https://cards-api-swart.vercel.app';
-export const HOST = `http://localhost:4000`;
+export const HOST = 'https://cards-api-swart.vercel.app';
+// export const HOST = `http://localhost:4000`;
